152bd4dddfecc425bbb854bac3800527de25da90
erwan Leboucher <erwanleboucher@gmail.com>
2015-02-10 17:04:03 +0100

    Update Bliss.mk

--- Bliss.mk
+++ Bliss.mk
@@ -68,7 +68,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_tf101
+PRODUCT_NAME := bliss_tf101
 PRODUCT_DEVICE := tf101
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Transformer TF101
